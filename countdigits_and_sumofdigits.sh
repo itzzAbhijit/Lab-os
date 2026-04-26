98e44aa422a3a432025e3ad71873409db0f2cba6
Abhijit Mohanty <abhijitmohanty334@gmail.com>
2026-04-26 22:31:08 +0530

    Create countdigits_and_sumofdigits.sh

---
+++ countdigits_and_sumofdigits.sh
@@ -0,0 +1,18 @@
+# Program 12 : Count Digits and Sum of Digits
+
+echo "Enter a number:"
+read num
+
+count=0
+sum=0
+
+while [ $num -gt 0 ]
+do
+    digit=$((num % 10))
+    sum=$((sum + digit))
+    count=$((count + 1))
+    num=$((num / 10))
+done
+
+echo "Number of digits = $count"
+echo "Sum of digits = $sum"
