f2e2860ff76d5a4c8068993a687fef967812d6fb
Abhijit Mohanty <abhijitmohanty334@gmail.com>
2026-04-26 22:29:57 +0530

    Create Armstrong.sh

---
+++ Armstrong.sh
@@ -0,0 +1,21 @@
+# Program 11 : Armstrong Number
+
+echo "Enter a number:"
+read num
+
+temp=$num
+sum=0
+
+while [ $num -gt 0 ]
+do
+    rem=$((num % 10))
+    sum=$((sum + rem*rem*rem))
+    num=$((num / 10))
+done
+
+if [ $temp -eq $sum ]
+then
+    echo "$temp is an Armstrong Number"
+else
+    echo "$temp is not an Armstrong Number"
+fi
